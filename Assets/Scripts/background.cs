6a0fbb0cc6e15fee46e1915445ba5ca4eaa37a5b
Caballo <ballo@mingoideas.mx>
2015-07-25 03:00:12 -0500

    fucking item and background interator

--- Assets/Scripts/background.cs
+++ Assets/Scripts/background.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class background : MonoBehaviour {
 
-	private var madera : GameObject;
+	private float vel = 10f;
+
+	private GameObject floor1;
+	private GameObject floor2;
 
 	// Use this for initialization
 	void Start () {
-		
+		/*floor1 = GameObject.Find("floor1");
+		Debug.Log(floor1);
+
+		floor2 = GameObject.Find("floor2");
+		Debug.Log(floor2);*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		/*float translation = Time.deltaTime * vel;
+		if (floor1.transform.position)
+        floor1.transform.Translate(-translation, 0, 0);
+        floor2.transform.Translate(-translation, 0, 0);*/
 	}
 }
---
+++ Assets/Scripts/background/ItemScroller.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemScroller : MonoBehaviour {
+
+	public float scrollSpeed;
+	public float repeatProbability;
+	public float outSize;
+	private float probMax = 1000;
+	private bool appear = false;
+	private float started = 0;
+
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Debug.Log(newPosition);
+        /*if (transform.position!=startPosition){
+        	
+        }else if (Random.value*probMax>probMax-repeatProbability){
+        	newPosition = scrollSpeed;
+        }*/
+        if (appear){
+        	float newPosition = Mathf.Repeat((Time.time-started) * scrollSpeed, outSize+scrollSpeed);
+        	if (newPosition>=outSize){
+        		appear = false;
+        	}
+			transform.position = startPosition + Vector3.left * newPosition;
+        }else if (Random.value*probMax>probMax-repeatProbability){
+        	started = Time.time;
+        	appear = true;
+        }
+	}
+}
