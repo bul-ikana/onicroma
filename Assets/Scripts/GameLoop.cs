d5f849a29ecc622310e6351ad8d9bc16afa28499
Caballo <ballo@mingoideas.mx>
2015-07-25 09:45:55 -0500

    revive player

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameLoop : MonoBehaviour {
 	private GameObject bar;
 	private GameObject kid;
+	private bool playing = true;
 	public static float scrollSpeed = 10;
 
 	// Use this for initialization
@@ -14,9 +15,17 @@ public class GameLoop : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(bar.GetComponent<HealthBar>().doDamage(0.01f)){
-			GameLoop.scrollSpeed = 0;
-			kid.GetComponent<Player>().playerDie();
+		if (playing){
+			if(bar.GetComponent<HealthBar>().doDamage(0.01f)){
+				GameLoop.scrollSpeed = 0;
+				kid.GetComponent<Player>().playerDie();
+				playing = false;
+			}
+		}else if (Input.GetMouseButtonDown(0)){
+			bar.GetComponent<HealthBar>().heal(.5f);
+			GameLoop.scrollSpeed = 10;
+			kid.GetComponent<Player>().playerBorn();
+			playing = true;
 		}
 	}
 }
