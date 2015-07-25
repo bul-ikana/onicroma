673d80fd43df395796ea0ebfb72e8ebb2857732c
Caballo <ballo@mingoideas.mx>
2015-07-25 04:00:29 -0500

    manchas y assets de Y cambiante

--- Assets/Scripts/background/ItemScrollerY.cs
+++ Assets/Scripts/background/ItemScrollerY.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ItemScroller : MonoBehaviour {
+public class ItemScrollerY : MonoBehaviour {
 
 	public float scrollSpeed;
 	public float repeatProbability;
 	public float outSize;
+	public float maxY;
+
 	private float probMax = 1000;
 	private bool appear = false;
 	private float started = 0;
 
 	private Vector3 startPosition;
+	private Vector3 newPositionVector;
 
 	// Use this for initialization
 	void Start () {
@@ -25,9 +28,10 @@ public class ItemScroller : MonoBehaviour {
         	if (newPosition>=outSize){
         		appear = false;
         	}
-			transform.position = startPosition + Vector3.left * newPosition;
+			transform.position = newPositionVector + Vector3.left * newPosition;
         }else if (Random.value*probMax>probMax-repeatProbability){
         	started = Time.time;
+        	newPositionVector = startPosition + Vector3.up * (Random.value*maxY);
         	appear = true;
         }
 	}
